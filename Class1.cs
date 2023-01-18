09c34e5f7a7b59dd778c9bac065895622df8dd0d
beent <beent@yandex.ru>
2023-01-18 20:43:22 +0300

    Add class1

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_Git_GarbageApp
+{
+    internal class Class1
+    {
+    }
+}
